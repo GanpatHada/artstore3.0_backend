162a8ad8a18101387994869fdef87d160ff8dd7a
GanpatHada <hadaganpat42@gmail.com>
2024-08-13 22:59:20 +0530

    fix:cors in app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const helmet=require('helmet');
 const cors=require('cors');
 const app=express();
-const PORT=3000||process.env.PORT;
+const PORT=8000||process.env.PORT;
 require('./db.js')
 const userRouter=require('./routes/userRoute.js');
 app.use(express.json())
