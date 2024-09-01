f235cd504d4eaee54576d58211f66032a0bcd25f
GanpatHada <hadaganpat42@gmail.com>
2024-09-01 20:47:44 +0530

    feat:fetch products api

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,11 +5,22 @@ async function addProduct(productDetails) {
     const product = new Product(productDetails);
     const savedProduct = await product.save();
     if (savedProduct)
-      return { status: 200, message: "product added successfuly" };
-    return { status: 401, message: "something went wrong" };
+      return { status: 201, message: "product added successfully",success:true };
+    return { status: 401, message: "something went wrong",status:false };
   } catch (error) {
     throw error;
   }
 }
 
-module.exports = { addProduct };
+async function getProducts(){
+  try {
+    const products=await Product.find();
+    if(products)
+      return {status:200,message:"products fetched successfully",success:true,data:products};
+    return {status:401,message:"products not found",status:false}
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { addProduct,getProducts };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,24 @@
 const express=require('express');
-const { addProduct } = require('../controllers/productController');
+const { addProduct, getProducts } = require('../controllers/productController');
 const productRouter=express.Router();
 
 productRouter.post("/",async(req,res)=>{
     try {
         const result=await addProduct(req.body);
-        res.status(result.status).json({message:result.message});
+        return res.status(result.status).json({message:result.message});
+    } catch (error) {
+        res.status(500).json({message:"Internal server error"});
+    }
+})
+
+productRouter.get("/",async(req,res)=>{
+    try {
+        const result=await getProducts();
+        if(result.success)
+            return res.status(result.status).json({message:result.message,data:result.data})
+        else
+           return res.status(result.status).json({message:result.message})
+
     } catch (error) {
         res.status(500).json({message:"Internal server error"});
     }
