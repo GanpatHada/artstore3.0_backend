fe487d958ddfc37db294931582209319c74c77e8
GanpatHada <hadaganpat42@gmail.com>
2024-09-15 21:37:51 +0530

    feat : added payment gateway

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const Razorpay=require('razorpay')
 const { createUser, userLogin } = require("../controllers/userController");
 const homeRouter = express.Router();
 
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY,
+  key_secret: process.env.RAZORPAY_SECRET
+});
 
+homeRouter.post('/create-order', async (req, res) => {
+  const { amount, currency, receipt } = req.body;
+  if (!amount)
+    return res
+      .status(400)
+      .json({ message: "amount is required" });
+  const options = {
+    amount: amount * 100,
+    currency: currency || "INR",
+    receipt: receipt || "order_rcptid_11",
+    payment_capture: 1
+  };
+
+  try {
+    const order = await razorpay.orders.create(options);
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).send("Error creating order");
+  }
+});
 
 homeRouter.post("/signup", async (req, res) => {
   try {
