92d8da2cf8fd8695b2090ad3c2ac36c9a02d10f7
GanpatHada <hadaganpat42@gmail.com>
2025-01-17 15:26:52 +0530

    fix : cookies error

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ app.use(express.urlencoded({extended:true,limit:'16kb'}));
 app.use(express.static('public'));
 app.use(cookieParser())
 app.use(helmet());
-app.use(cors());
+app.use(cors(
+    {
+        origin: "*",
+        credentials: true
+    }
+));
 
 app.use("/", homeRouter);
 app.use("/api/v1/user", userRouter);
