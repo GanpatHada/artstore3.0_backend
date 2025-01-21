97c5c3359199a58aea9bce58338dab32844c9e0b
GanpatHada <hadaganpat42@gmail.com>
2025-01-21 15:24:19 +0530

    fix : error api message format

--- src/middlewares/errorhandler.js
+++ src/middlewares/errorhandler.js
@@ -3,13 +3,10 @@ function errorHandler(error, req, res, next) {
   res
     .status(error.statusCode || 500)
     .json({
-      message: "Internal server error",
+      message: error.message,
       data: error.data,
       success: error.success,
-      error:{
-        code:error.code,
-        details:error.message
-      }
+      code :error.code
     });
 }
 module.exports = errorHandler;
