7f0b73285f80f05db30f5e5044dcef7e9675640f
GanpatHada <hadaganpat42@gmail.com>
2025-03-12 15:28:47 +0530

    fix : cors error

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -14,23 +14,29 @@ const loginValidation = Joi.object({
 });
 
 const registerValidation = Joi.object({
-  email: Joi.string().email().required().messages({
+  email: Joi.string().email().trim().required().messages({
     "string.email": "Invalid email format",
     "any.required": "Email is required",
-    "string.empty": "Email is empty",
+    "string.empty": "Email cannot be empty",
   }),
-  password: Joi.string().min(6).required().messages({
+  password: Joi.string().min(6).trim().required().messages({
+    "string.min": "Password must be at least 6 characters long",
     "any.required": "Password is required",
-    "string.empty": "Password is empty",
+    "string.empty": "Password cannot be empty",
   }),
-  fullName: Joi.string().email().required().messages({
+  fullName: Joi.string().trim().required().messages({
     "any.required": "Name is required",
-    "string.empty": "Name is empty",
+    "string.empty": "Name cannot be empty",
   }),
-  Phone: Joi.string().min(6).required().messages({
-    "any.required": "Phone number is required",
-    "string.empty": "Phone number is empty",
-  }),
+  phone: Joi.string()
+    .pattern(/^\d{10}$/)
+    .trim()
+    .required()
+    .messages({
+      "string.pattern.base": "Phone number must be 10 digits",
+      "any.required": "Phone number is required",
+      "string.empty": "Phone number cannot be empty",
+    }),
 });
 
-module.exports = { loginValidation,registerValidation };
+module.exports = { loginValidation, registerValidation };
